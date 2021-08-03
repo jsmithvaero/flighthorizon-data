0d298ab30c2527539e31f31373cce6fd250d3828
jsmithvaero <73196008+jsmithvaero@users.noreply.github.com>
2021-08-03 16:36:14 -0500

    No changes (adding file)

---
+++ src/Point.py
@@ -0,0 +1,8 @@
+"""
+file       : Point.py
+author     : Max von Hippel, but not really, it's Levi's brilliant idea :D
+authored   : 31 July 2021
+description: Better than a tuple!
+"""
+class Point(object):
+	pass
