3a431a0878bee7a7e277a323e1ca5aa411fa8b20
jsmithvaero <73196008+jsmithvaero@users.noreply.github.com>
2021-08-03 20:58:18 -0500

    a little formatting

--- runner.py
+++ runner.py
@@ -7,28 +7,29 @@ usage   : python3 runner.py demo-data
 import sys
 import argumentKeys as argKeys
 
-from src.radarData                  import RadarData
-from src.radarDataGroundAware       import RadarDataGroundAware
-from src.blocks                     import radarTruthBlocks
-from src.truthData                  import *
-from src.questions.Question         import *
-from src.questions.QuestionPcts     import *
+from src.radarData import RadarData
+from src.radarDataGroundAware import RadarDataGroundAware
+from src.blocks import radarTruthBlocks
+from src.truthData import *
+from src.questions.Question import *
+from src.questions.QuestionPcts import *
 
 TRIVIAL_THRESHOLD = 4
+
 
 def main():
 
     args = argKeys.parse()
     argKeyList = list(vars(args).keys())
 
-     # We begin by finding all of the data.
-    input_folder = args.folder[0]   
+    # We begin by finding all of the data.
+    input_folder = args.folder[0]
 
-    radarD = RadarData  (input_folder)
-    adsbD  = ADSBData   (input_folder)
-    nmeaD  = NMEAData   (input_folder)
-    gpxD   = GPXData    (input_folder)
-    mavlD  = MavlinkData(input_folder)
+    radarD = RadarData(input_folder)
+    adsbD = ADSBData(input_folder)
+    nmeaD = NMEAData(input_folder)
+    gpxD = GPXData(input_folder)
+    mavlD = MavlinkData(input_folder)
 
     truthD = adsbD.union(nmeaD)\
                   .union(gpxD)\
@@ -38,10 +39,11 @@ def main():
     if "percents" in argKeyList:
 
         # for GroundAware
-        radarDGA = RadarDataGroundAware (input_folder)
+        radarDGA = RadarDataGroundAware(input_folder)
         questionPcts = QuestionPcts()
         questionPcts.invalidvsValidTracksGroundAware(radarDGA)
-        questionPcts.radarAccuracyByDistanceAltitudeGroundAware(mavlD, radarDGA)
+        questionPcts.radarAccuracyByDistanceAltitudeGroundAware(
+            mavlD, radarDGA)
 
         # for Echodyne
         questionPcts.invalidvsValidTracks(radarD)
@@ -53,22 +55,23 @@ def main():
     BLOCKS = radarTruthBlocks(radarD.getPoints(), truthD.getPoints())
 
     BLOCKED_DATAS = [
-        (RadarData(folder=None,points=radarBlock),
-         TruthData(folder=None,points=truthBlock))
-        for (radarBlock, truthBlock)
-        in BLOCKS
+        (RadarData(folder=None, points=radarBlock),
+         TruthData(folder=None, points=truthBlock))
+
+        for (radarBlock, truthBlock) in BLOCKS
     ]
 
     # Remove singletons and empty sets.
     BLOCKED_DATAS = [
+
         (RD, TD) for (RD, TD) in BLOCKED_DATAS
+
         if RD.isNonTrivial(TRIVIAL_THRESHOLD) and
-        TD.isNonTrivial(TRIVIAL_THRESHOLD)
+            TD.isNonTrivial(TRIVIAL_THRESHOLD)
     ]
 
     # Finally, let's answer some questions, over the various blocks.
     for (RD, TD) in BLOCKED_DATAS:
-
         for countI, (independent_parser, independent_name) in enumerate(INDEPENDENTS):
             for countD, (dependent_parser, dependent_name) in enumerate(DEPENDENTS):
                 if countI in args.independents and countD in args.dependents:
@@ -85,6 +88,7 @@ def main():
                         answer.plotXY(save=True)
 
     print("DONE")
+
 
 if __name__ == "__main__":
     main()
